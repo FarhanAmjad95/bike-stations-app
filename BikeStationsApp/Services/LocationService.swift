301a8cafde0f049859163772109ca4e39be02687
Farhan <>
2024-09-13 22:45:04 +0200

    removed extra file

--- BikeStationsApp/Services/LocationService.swift
+++ BikeStationsApp/Services/LocationService.swift
@@ -71,3 +71,34 @@ class LocationService: NSObject, LocationServiceProtocol, CLLocationManagerDeleg
         case noLocationFound
     }
 }
+
+
+class MockLocationService: LocationServiceProtocol {
+    // Publisher for authorization status
+    private let authorizationStatusSubject = CurrentValueSubject<CLAuthorizationStatus, Never>(.notDetermined)
+    
+    // Mock properties for testing
+    var mockLocation: CLLocation?
+    var mockError: Error?
+    var mockAuthorizationStatus: CLAuthorizationStatus = .notDetermined
+    
+    var authorizationStatus: AnyPublisher<CLAuthorizationStatus, Never> {
+        return authorizationStatusSubject.eraseToAnyPublisher()
+    }
+   
+    // Simulates requesting the current location
+    func requestLocation() async throws -> CLLocation {
+        return try await withCheckedThrowingContinuation { continuation in
+            if let mockLocation = mockLocation {
+                continuation.resume(returning: mockLocation)
+            } else if let mockError = mockError {
+                continuation.resume(throwing: mockError)
+            } else {
+                // Provide a default location for testing
+                let location = CLLocation(latitude: Constants.Location.defaultLatitude, longitude: Constants.Location.defaultLongitude)
+                mockLocation = location
+                continuation.resume(returning: location) // Vienna coordinates
+            }
+        }
+    }
+}
--- BikeStationsApp/Services/NetworkService.swift
+++ BikeStationsApp/Services/NetworkService.swift
@@ -34,3 +34,25 @@ class NetworkService: NetworkServiceProtocol {
         }
     }
 }
+
+
+class MockNetworkService: NetworkServiceProtocol {
+    var result: Result<[BikeStation], Error>?
+    
+    init(result: Result<[BikeStation], Error>? = nil) {
+        self.result = result
+    }
+    
+    func fetchBikeStations() async throws -> [BikeStation] {
+        if let result = result {
+            switch result {
+            case .success(let stations):
+                return stations
+            case .failure(let error):
+                throw error
+            }
+        } else {
+            throw NSError(domain: "MockNetworkServiceError", code: 0, userInfo: [NSLocalizedDescriptionKey: "No result available"])
+        }
+    }
+}
